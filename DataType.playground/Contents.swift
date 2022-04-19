c18d630f974f5e53ba517ff6e614c2cbd79900db
‘ITNBIZ-주문-안재영’ <ajy02@lgupluspartners.co.kr>
2022-04-19 15:40:04 +0900

    데이터형

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+var mesaage : String = "Hello ~ "
+
+var isLogin : Bool = false
+
+var day: Int = 27
+
+var weight: Double = 65.612341234
+
+var height: Float = 175.5123412341234123412
+
+UInt8.min
+UInt8.min
+
+Int8.min
+Int8.max
+
+Int16.min
+Int16.max
+
+Int32.min
+Int32.max
+
+Int64.min
+Int64.max
+
---
+++ infleanStudy.playground/Contents.swift
@@ -0,0 +1,18 @@
+var radius = 3.123
+
+let pi = 3.1415926535
+
+/*
+ 
+ */
+radius * radius * pi
+
+
+
+radius = 1.23
+
+radius * radius * pi;
+
+func circleArea(r: Double) -> Double {
+    return r * r *
+}
